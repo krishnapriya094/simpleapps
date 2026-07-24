e507429b5acff3e1e3ced9832ea4a0ef68fc12a7
krishnapri094 <25mx115@psgtech.ac.in>
2026-07-24 10:10:56 +0530

    Added min and max feature to sorting app

--- sorting.py
+++ sorting.py
@@ -3,5 +3,10 @@ numbers = [45, 12, 89, 23, 7, 56]
 print("Original List:", numbers)
 
 numbers.sort()
+print("Ascending Order:", numbers)
 
-print("Sorted List:", numbers)
+numbers.sort(reverse=True)
+print("Descending Order:", numbers)
+
+print("Smallest number:", min(numbers))
+print("Largest number:", max(numbers))
